80f08fa8b49eb3799d48927678c8c485bb5bb67c
Azamman327 <wjsdbdud327@naver.com>
2025-05-11 18:13:46 +0900

    📦Chore: 프로젝트 초기세팅

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import User from "./pages/user";
+import Board from "./pages/board";
+import Chat from "./pages/chat";
+import Map from "./pages/map";
+import MyPage from "./pages/mypage";
+
+import "./App.css";
+
+function App() {
+  function setScreenSize() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  useEffect(() => {
+    setScreenSize();
+  });
+
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50 layout">
+        <Routes>
+          <Route path="/" element={<User />} />
+          <Route path="/board" element={<Board />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
