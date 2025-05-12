b7e5567c91ad2e38a4418471d6c32a0d70193c20
Azamman327 <wjsdbdud327@naver.com>
2025-05-12 16:58:52 +0900

    ♻️Refactor: live-map 지도,마커 생성 코드 리팩토링

--- src/pages/map/index.jsx
+++ src/pages/map/index.jsx
@@ -5,33 +5,40 @@ import LiveMapLoader from "./liveMapLoader";
 const LiveMap = () => {
   const mapRef = useRef(null);
 
+  function createMap() {
+    const mapContainer = mapRef.current;
+    const options = {
+      center: new window.kakao.maps.LatLng(37.5665, 126.9780),
+      level: 3,
+    };
+    return new window.kakao.maps.Map(mapContainer, options);
+  }
+
+  function createMarkers(map, bounds) {
+    const points = [
+      new window.kakao.maps.LatLng(37.5665, 126.9780),
+      new window.kakao.maps.LatLng(37.5665, 126.975)
+    ];
+
+    for (let i = 0; i < points.length; i++) {
+      const marker = new window.kakao.maps.Marker({ position : points[i] });
+      marker.setMap(map);
+
+      bounds.extend(points[i]);
+    }
+  }
+
   useEffect(() => {
     LiveMapLoader().then(() => {
       window.kakao.maps.load(() => {
-        const mapContainer = mapRef.current;
-        const options = {
-          center: new window.kakao.maps.LatLng(37.5665, 126.9780),
-          level: 3,
-        };
-        const map = new window.kakao.maps.Map(mapContainer, options);
-
-        const points = [
-          new window.kakao.maps.LatLng(37.5665, 126.9780),
-          new window.kakao.maps.LatLng(37.5665, 126.975)
-        ];
+        const map = createMap();
 
         function setBounds() {
           map.setBounds(bounds);
         }
-
         const bounds = new window.kakao.maps.LatLngBounds();  
 
-        for (let i = 0; i < points.length; i++) {
-          const marker = new window.kakao.maps.Marker({ position : points[i] });
-          marker.setMap(map);
-
-          bounds.extend(points[i]);
-        }
+        createMarkers(map, bounds)
 
         setBounds()
       });
