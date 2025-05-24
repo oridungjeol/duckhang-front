5c03571d0ad898acc6cddaf3699a27f678e0b8ba
Azamman327 <wjsdbdud327@naver.com>
2025-05-24 19:09:14 +0900

    🐛Fix: chatroomList usestate 초기값 오류 수정

--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -8,41 +8,16 @@ import "./index.css"
 export default function Chat() {
 
   const navigate = useNavigate();
-  const [datas, setDatas] = useState({})
+  const [datas, setDatas] = useState([])
 
   useEffect(() => {
     const fetchChatRoomList = async() => {
       const chatRoomList = await getChatRoomList();
-      console.log(chatRoomList);
       setDatas(chatRoomList);
     }
 
     fetchChatRoomList();
   }, [])
-
-  // const datas = [
-  //   {
-  //     "room_id": 555,
-  //     "name": "채팅방1",  //참여자 이름
-  //     "recent": "최근 대화1",
-  //     "boardId": 102,
-  //     "type": "rental"
-  //   },
-  //   {
-  //     "room_id": 666,
-  //     "name": "채팅방2",
-  //     "recent": "최근 대화2",
-  //     "boardId": 102,
-  //     "type": "rental"
-  //   },
-  //   {
-  //     "room_id": 777,
-  //     "name": "채팅방3",
-  //     "recent": "최근 대화3",
-  //     "boardId": 102,
-  //     "type": "rental"
-  //   },
-  // ]
 
   const enterChatRoom = (data) => {
     navigate(`/chat/${data.room_id}`, { state: data });
@@ -52,8 +27,8 @@ export default function Chat() {
     <div className="container">
       <h1>채팅</h1>
       <div className="chatting-list">
-        {datas.map((data, index) => (
-          <div key={index} className="chatting" onClick={() => { enterChatRoom(data) }}>
+        {datas.map((data) => (
+          <div key={data.room_id} className="chatting" onClick={() => { enterChatRoom(data) }}>
             <div>
               <img src="/images/duckhang.jpg" alt="duckhang" className="chat-thumbnail" />
             </div>
