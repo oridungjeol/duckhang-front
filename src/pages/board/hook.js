67cb814feafa47c536aed0c551b7971b4d920c07
Azamman327 <wjsdbdud327@naver.com>
2025-05-25 17:09:50 +0900

    ✨Feat: 채팅방 생성

---
+++ src/pages/board/hook.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+/**
+ * 새로운 채팅방을 생성합니다.
+ * @param {*} name 채팅방 이름
+ * @param {*} board_id 게시글 고유 번호
+ * @param {*} type 게시글 타입
+ * @returns 채팅방 정보
+ */
+export async function createChatRoom(name, board_id, type) {
+  const data = {
+    name: name,
+    board_id: board_id,
+    type: type,
+  };
+
+  try {
+    const response = await axios.post(
+      `http://localhost/api/chat/create`,
+      data,
+      {
+        withCredentials: true,
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("채팅방 생성 중 오류 발생:", error);
+  }
+}
